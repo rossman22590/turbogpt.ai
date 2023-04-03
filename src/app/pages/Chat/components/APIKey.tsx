031ffa43e1a39d7c5a81671a152ad225dc04bd3c
Ross Cohen <rosscohen1990@gmail.com>
2023-04-03 02:04:15 -0400

    Update APIKey.tsx
    Signed-off-by: Ross Cohen <rosscohen1990@gmail.com>

--- src/app/pages/Chat/components/APIKey.tsx
+++ src/app/pages/Chat/components/APIKey.tsx
@@ -19,7 +19,9 @@ import { saveOpenAiKey } from '../utils';
 export function APIKey() {
   const openAiApiKey = process.env.OPENAI_API_KEY;
   const [error, setError] = React.useState<string | null>(null);
-    const [apiKey, setApiKey] = process.env.OPENAI_API_KEY, ;
+const [apiKey, setApiKey] = React.useState<string>(
+    openAiApiKey,
+  );
   const dispatch = useDispatch();
   const { actions } = useChatOptionsSlice();
 
